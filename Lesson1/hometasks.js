a1d1478715a35de8e2c5c0d58d1fdd28cd96bbec
Raman Davidovich <raman.davidovich@gmail.com>
2024-07-07 16:47:39 +0300

    feat: added descriptions for lesson 7 tasks

--- Lesson1/hometasks.js
+++ Lesson1/hometasks.js
@@ -131,6 +131,17 @@ const tasks = [
         "https://github.com/raman-davidovich/Andersen_traineeship/blob/main/Lesson1/solutions/lesson6Task1.js",
     },
   ],
+  [
+    {
+      description: `In the center of the page there is a button that says Click me! When you hover it with  50% probability, it disappears and appears in a random place. When clicked, it does the same with 100% probability. `,
+      githubLink:
+        "https://github.com/raman-davidovich/Andersen_traineeship/blob/main/Lesson1/solutions/lesson7Task1.js",
+    },
+    {
+      description: `The page consists of two halves: a stopwatch and a timer. The functionality is appropriate, you can receive data for the timer simply by text input, or you can use buttons like "+1m.". <br/>Accurate down to seconds.`,
+      githubLink: "",
+    },
+  ],
 ];
 
 export default tasks;
